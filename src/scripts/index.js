8c42d719013514d391339d301b5059c13602a789
biscake <leonut.ljh@gmail.com>
2024-06-03 00:31:52 +0800

    Initial commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,6 @@
-import "/src/css/reset.css";
-import "/src/css/style.css";
+// import "/src/css/reset.css";
+// import "/src/css/style.css";
+
+export default function addOneToX(x) {
+  return x + 1;
+}
